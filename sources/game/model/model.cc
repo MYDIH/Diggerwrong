ecfef6aa8cd0e1d279075b1538f0f4e4c3d23944
Nicolas <nicgomez@localhost.localdomain>
2014-03-20 17:17:34 +0100

    L'affichage console quasi terminé

--- sources/game/model/model.cc
+++ sources/game/model/model.cc
@@ -6,73 +6,73 @@
 using namespace diggewrong;
 
 GameModel::GameModel(unsigned width, unsigned height, double difficulty
-		     ,unsigned target, unsigned lifes)
+                     ,unsigned target, unsigned lifes)
 
 
-   :Board(width)
+    :Board(width)
 
-   ,Digger({width/2, height/2})
+    ,Digger({width/2, height/2})
 
-   ,Target(target)
-   ,Reached(0)
+,Target(target)
+,Reached(0)
 
-   ,Score(0)
-   ,Lifes(lifes)
-    //,Timelimit(timelimit)
+,Score(0)
+,Lifes(lifes)
+//,Timelimit(timelimit)
 
-   ,Bonus_score(0)
-   ,Bonus_lifes(0)
-    //,Bonus_time(0)
+,Bonus_score(0)
+,Bonus_lifes(0)
+//,Bonus_time(0)
 {
-   const unsigned longestside = (width > height) ? width : height;
+    const unsigned longestside = (width > height) ? width : height;
 
-   for (auto & column : Board)
-   {
-      column.resize(height);
+    for (auto & column : Board)
+    {
+        column.resize(height);
 
-      for (auto & square : column)
-      {
-	      square = newRandomSquare(difficulty, longestside);
-      }
-   }
+        for (auto & square : column)
+        {
+            square = newRandomSquare(difficulty, longestside);
+        }
+    }
 }
 
 GameModel::~GameModel()
 {
-   for (auto & column : Board)
-   {
-      for (auto & square : column)
-      {
-	      square -> release();
-      }
-   }
+    for (auto & column : Board)
+    {
+        for (auto & square : column)
+        {
+            square -> release();
+        }
+    }
 }
 
 
 bool GameModel::move(int dx, int dy)
 {
-   if ( digAt(Digger.x + dx, Digger.y + dy, dx, dy) )
-   {
+    if ( digAt(Digger.x + dx, Digger.y + dy, dx, dy) )
+    {
 
-      return true;
-   }
-   else
-   {
+        return true;
+    }
+    else
+    {
 
-      return false;
-   }
+        return false;
+    }
 }
 
 Square* GameModel::newRandomSquare(double difficulty, unsigned longestside)
 {
-   double r = rand() / (double) RAND_MAX;
+    double r = rand() / (double) RAND_MAX;
 
-   double pbomb  = difficulty * 0.7;
-   double pbonus = (1 - pbomb) * 0.2;
+    double pbomb  = difficulty * 0.7;
+    double pbonus = (1 - pbomb) * 0.2;
 
-   if      (r <= pbomb)          return new Normal(difficulty, longestside); // bomb
-   else if (r <= pbomb + pbonus) return new Normal(difficulty, longestside); // bonus
-   else                          return new Normal(difficulty, longestside); // normal
+    if      (r <= pbomb)          return new Normal(difficulty, longestside); // bomb
+    else if (r <= pbomb + pbonus) return new Normal(difficulty, longestside); // bonus
+    else                          return new Normal(difficulty, longestside); // normal
 }
 
 const std::string GameModel::toString() const
@@ -80,19 +80,29 @@ const std::string GameModel::toString() const
     std::string tempString = "";
     std::string tempLine = "";
 
+    tempLine += "+";
     for(unsigned i=0; i<Board[0].size(); i++)
-      tempLine += '-';
+        tempLine += "---";
+    tempLine += "+";
 
     for(unsigned i=0; i<Board.size(); i++)
     {
         for(unsigned j=0; j<Board[i].size(); j++)
         {
-          if(!(i % 2))
-            tempString += tempLine + '\n';
-          tempString = '|' + Board[i][j]->toString() + '|';
-          if(j == Board[i].size() - 1)
-            tempString += '\n';
+            if(i == 0 && j == 0)
+                tempString += tempLine + '\n';
+            if(j == 0)
+                tempString += '|';
+            if(i != Digger.x || j != Digger.y)
+                tempString += Board[i][j]->toString();
+            else
+                tempString += "@@@";
+            if(j == Board[i].size() - 1)
+                tempString += '|';
         }
+        tempString += '\n';
+        if(i == Board.size() - 1)
+            tempString += tempLine;
     }
 
     return tempString;
@@ -100,53 +110,74 @@ const std::string GameModel::toString() const
 
 std::string GameModel::intToString(const int &e)
 {
-  std::ostringstream stream;
-  stream << e;
+    std::ostringstream stream;
+    stream << e;
 
-  return stream.str();
+    return stream.str();
 }
 
 bool GameModel::isOutOfRange(int x, int y)
 {
-   if (x >= 0 and y >= 0 and (unsigned)x < Board.size() and (unsigned)y < Board[0].size())
-      return false;
-   else
-      return true;
+    if (x >= 0 and y >= 0 and (unsigned)x < Board.size() and (unsigned)y < Board[0].size())
+        return false;
+    else
+        return true;
 }
 
 bool GameModel::digAt(int x, int y
-		      ,int dx, int dy, int distance)
+                      ,int dx, int dy, int distance)
 {
-   if (isOutOfRange(x,y))
-   {
-      return true;
-   }
-   else
-   {
-      Digger.x = x;
-      Digger.y = y;
-      Reached++;
+    if (isOutOfRange(x,y))
+    {
+        return true;
+    }
+    else
+    {
+        Digger.x = x;
+        Digger.y = y;
+        Reached++;
 
-      Board[x][y] -> retain();
-      const bool ret = Board[x][y] -> dig(*this,x,y,dx,dy,distance);
-      Board[x][y] -> release();
+        Board[x][y] -> retain();
+        const bool ret = Board[x][y] -> dig(*this,x,y,dx,dy,distance);
+        Board[x][y] -> release();
 
-      return ret;
-   }
+        return ret;
+    }
 }
 
 void GameModel::addScore(unsigned score)
 {
-   Score += score;
+    Score += score;
 }
 
 void GameModel::replaceSquare(int x, int y, Square * newone)
 {
-   if (not isOutOfRange(x,y))
-   {
-      Board[x][y] -> release();
-      newone -> retain();
-      Board[x][y] = newone;
-   }
+    if (not isOutOfRange(x,y))
+    {
+        Board[x][y] -> release();
+        newone -> retain();
+        Board[x][y] = newone;
+    }
 }
 
+// Accesseurs :
+
+unsigned GameModel::getTarget()
+{
+    return Target;
+}
+
+unsigned GameModel::getReached()
+{
+    return Reached;
+}
+
+unsigned GameModel::getScore()
+{
+    return Score;
+}
+
+unsigned GameModel::getLifes()
+{
+    return Lifes;
+}
--- sources/main.cc
+++ sources/main.cc
@@ -1,15 +1,71 @@
 #include "GameModel.hh"
 #include "GameObserver.hh"
 #include <cstdlib>
+#include <string>
 #include <iostream>
 
 using namespace diggewrong;
 
+void afficherMenu()
+{
+    std::cout << "Pour se déplacer dans les 8 directions : O,N,E,S,NO,NE,SO,SE" << std::endl
+              << "Quitter le jeu : Q" << std::endl;
+}
+
+bool menu(GameModel *model)
+{
+    std::string tempStr = "";
+
+    afficherMenu();
+    std::cin >> tempStr;
+
+    if(tempStr == "O")
+    {}
+    else if(tempStr == "N")
+    {}
+    else if(tempStr == "E")
+    {}
+    else if(tempStr == "S")
+    {}
+    else if(tempStr == "NO")
+    {}
+    else if(tempStr == "NE")
+    {}
+    else if(tempStr == "SO")
+    {}
+    else if(tempStr == "SE")
+    {}
+    else if(tempStr == "Q")
+        return true;
+    else
+    {
+        std::cout << "Veuillez entrer O,N,E,S,NO,NE,SO,SE ou Q !" << std::endl;
+        menu(model);
+    }
+}
+
+void printModel(GameModel *model)
+{
+    std::cout << model->toString() << std::endl
+              << std::endl
+              << "Cible a atteindre : " + GameModel::intToString(model->getTarget()) << std::endl
+              << "Nombre de déplacement : " + GameModel::intToString(model->getReached()) << std::endl
+              << "Votre Score : " + GameModel::intToString(model->getScore()) << std::endl
+              << "Vies Restantes : " + GameModel::intToString(model->getLifes()) << std::endl << std::endl;
+}
+
 int main()
 {
-    GameModel *model = new GameModel(18, 18, 0.5, 1, 5);
-    std::cout << model->toString().c_str() << std::endl;
-    std::cout << "Programme démarré" << std::endl;
+    bool quit = false;
+    GameModel *model = new GameModel(20, 20, 0.2, 20, 5);
+
+    while(!quit)
+    {
+        printModel(model);
+        quit = menu(model);
+    }
+
+    delete model;
 
     return EXIT_SUCCESS;
 }
