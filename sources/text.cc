5be76bf8a68992ca7c9dbb6ebbe16289cd9bce00
Timothée JOURDE <timothee.jourde@etu.u-bordeaux1.fr>
2014-04-28 13:56:43 +0200

    correction petite erreur ; cela dit le score est pas conservé entre les niveaux... tan pi

--- sources/text.cc
+++ sources/text.cc
@@ -7,8 +7,8 @@
 ////////////////////////////////////////////////
 void afficherMenu()
 {
-    std::cout << "Pour se déplacer dans les 8 directions : O,N,E,S,NO,NE,SO,SE ou 4,8,6,2,7,9,3,1" << std::endl
-              << "Quitter le jeu : Q" << std::endl;
+   std::cout << "Pour se déplacer dans les 8 directions : O,N,E,S,NO,NE,SO,SE ou 4,8,6,2,7,9,3,1" << std::endl
+	     << "Quitter le jeu : Q" << std::endl;
 }
 
 ///////////////toLowerCase()////////////////////
@@ -18,9 +18,9 @@ void afficherMenu()
 ////////////////////////////////////////////////
 void toLowerCase(std::string &s)
 {
-    std::locale loc;
-    for (std::string::size_type i=0; i<s.length(); ++i)
-        s[i] = std::tolower(s[i],loc);
+   std::locale loc;
+   for (std::string::size_type i=0; i<s.length(); ++i)
+      s[i] = std::tolower(s[i],loc);
 }
 
 ///////////////toLowerCase()////////////////////
@@ -34,61 +34,61 @@ void toLowerCase(std::string &s)
 ////////////////////////////////////////////////
 GameState menu(Board *model, unsigned &lifes)
 {
-    std::string tempStr = "";
-    bool rightStr = false;
-    GameState state;
+   std::string tempStr = "";
+   bool rightStr = false;
+   GameState state;
 
-    afficherMenu();
-    std::cin >> tempStr;
+   afficherMenu();
+   std::cin >> tempStr;
 
-    while(!rightStr) // Tant que la string envoyée est invalide
-    {
-        rightStr = true;
-        toLowerCase(tempStr); // On Décapitalise
+   while(!rightStr) // Tant que la string envoyée est invalide
+   {
+      rightStr = true;
+      toLowerCase(tempStr); // On Décapitalise
 
-        if(tempStr == "o" || tempStr == typeToString<int>(4))
-            state = model->move(0, -1);
-        else if(tempStr == "n" || tempStr == typeToString<int>(8))
-            state = model->move(-1, 0);
-        else if(tempStr == "e" || tempStr == typeToString<int>(6))
-            state = model->move(0, 1);
-        else if(tempStr == "s" || tempStr == typeToString<int>(2))
-            state = model->move(1, 0);
-        else if(tempStr == "no" || tempStr == typeToString<int>(7))
-            state = model->move(-1, -1);
-        else if(tempStr == "ne" || tempStr == typeToString<int>(9))
-            state = model->move(-1, 1);
-        else if(tempStr == "se" || tempStr == typeToString<int>(3))
-            state = model->move(1, 1);
-        else if(tempStr == "so" || tempStr == typeToString<int>(1))
-            state = model->move(1, -1);
-        else if(tempStr == "q")
-            return QUIT; // On quitte le jeu
-        else
-        {
-            std::cout << "Veuillez entrer O,N,E,S,NO,NE,SO,SE,4,8,6,2,7,9,3,1 ou Q !" << std::endl;
-            std::cin >> tempStr;
-            rightStr = false;
-        }
-    }
+      if(tempStr == "o" || tempStr == typeToString<int>(4))
+	 state = model->move(0, -1);
+      else if(tempStr == "n" || tempStr == typeToString<int>(8))
+	 state = model->move(-1, 0);
+      else if(tempStr == "e" || tempStr == typeToString<int>(6))
+	 state = model->move(0, 1);
+      else if(tempStr == "s" || tempStr == typeToString<int>(2))
+	 state = model->move(1, 0);
+      else if(tempStr == "no" || tempStr == typeToString<int>(7))
+	 state = model->move(-1, -1);
+      else if(tempStr == "ne" || tempStr == typeToString<int>(9))
+	 state = model->move(-1, 1);
+      else if(tempStr == "se" || tempStr == typeToString<int>(3))
+	 state = model->move(1, 1);
+      else if(tempStr == "so" || tempStr == typeToString<int>(1))
+	 state = model->move(1, -1);
+      else if(tempStr == "q")
+	 return QUIT; // On quitte le jeu
+      else
+      {
+	 std::cout << "Veuillez entrer O,N,E,S,NO,NE,SO,SE,4,8,6,2,7,9,3,1 ou Q !" << std::endl;
+	 std::cin >> tempStr;
+	 rightStr = false;
+      }
+   }
 
-    switch(state)
-    {
-        case WON :
-            std::cout << "-------------------------Vous Avez Gagné !-------------------------" << std::endl;
-            break;
-        case LOST :
-            std::cout << "-------------------------Vous Avez Perdu !-------------------------" << std::endl;
-            lifes--;
-            if(lifes < 1)
-                std::cout << "-----------------------------Game Over-----------------------------" << std::endl;
-            break;
-        case CONTINUE :
-            break;
-        default : {}
-    }
+   switch(state)
+   {
+      case WON :
+	 std::cout << "-------------------------Vous Avez Gagné !-------------------------" << std::endl;
+	 break;
+      case LOST :
+	 std::cout << "-------------------------Vous Avez Perdu !-------------------------" << std::endl;
+	 lifes--;
+	 if(lifes < 1)
+	    std::cout << "-----------------------------Game Over-----------------------------" << std::endl;
+	 break;
+      case CONTINUE :
+	 break;
+      default : {}
+   }
 
-    return state;
+   return state;
 }
 
 //////////////////printModel()////////////////////
@@ -98,101 +98,102 @@ GameState menu(Board *model, unsigned &lifes)
 //////////////////////////////////////////////////
 void printModel(Board *model, unsigned lifes, unsigned level, int charSet)
 {
-    std::string tempTarget = "Cible a atteindre : " + typeToString<int>(model->getTarget());        //
-    std::string tempReached = "Nombre de déplacement : " + typeToString<int>(model->getReached());  //
-    std::string tempScore = "Votre Score : " + typeToString<int>(model->getScore());                //
-    std::string tempScoreBonus = "Votre Score bonus : " + typeToString<int>(model->getBonusScore());//  On créé les lignes de statu
-    std::string lifeStr = "Vies Restantes : " + typeToString<int>(lifes);                             //
-    std::string levelStr = "Niveau n°" + typeToString<int>(level) + " (difficulté: " + typeToString<int>(inv(level)*100) + "%)";                                     //
+   std::string tempTarget = "Cible a atteindre : " + typeToString<int>(model->getTarget());        //
+   std::string tempReached = "Nombre de déplacement : " + typeToString<int>(model->getReached());  //
+   std::string tempScore = "Votre Score : " + typeToString<int>(model->getScore());                //
+   std::string tempScoreBonus = "Votre Score bonus : " + typeToString<int>(model->getBonusScore());//  On créé les lignes de statu
+   std::string lifeStr = "Vies Restantes : " + typeToString<int>(lifes);                             //
+   std::string levelStr = "Niveau n°" + typeToString<int>(level) + " (difficulté: " + typeToString<int>(inv(level)*100) + "%)";                                     //
 
-    // On rends le résultat potable en ramenant chaque ligne de statu à la même longueur
-    while(tempTarget.length() < DECALAGE_CONSOLE || tempReached.length() < DECALAGE_CONSOLE || tempScore.length() < DECALAGE_CONSOLE || tempScoreBonus.length() < DECALAGE_CONSOLE)
-    {
-        if(tempTarget.length() < DECALAGE_CONSOLE)
-            tempTarget += ' ';
-        if(tempReached.length() < DECALAGE_CONSOLE)
-            tempReached += ' ';
-        if(tempScore.length() < DECALAGE_CONSOLE)
-            tempScore += ' ';
-        if(tempScoreBonus.length() < DECALAGE_CONSOLE)
-            tempScoreBonus += ' ';
-    }
+   // On rends le résultat potable en ramenant chaque ligne de statu à la même longueur
+   while(tempTarget.length() < DECALAGE_CONSOLE || tempReached.length() < DECALAGE_CONSOLE || tempScore.length() < DECALAGE_CONSOLE || tempScoreBonus.length() < DECALAGE_CONSOLE)
+   {
+      if(tempTarget.length() < DECALAGE_CONSOLE)
+	 tempTarget += ' ';
+      if(tempReached.length() < DECALAGE_CONSOLE)
+	 tempReached += ' ';
+      if(tempScore.length() < DECALAGE_CONSOLE)
+	 tempScore += ' ';
+      if(tempScoreBonus.length() < DECALAGE_CONSOLE)
+	 tempScoreBonus += ' ';
+   }
 
-    // On inscrit des précisions sur les représentations au sein de la matrice a droite du statu
-    tempTarget += "|    \"" + CHARS[charSet][1] + "x" + CHARS[charSet][2] + "\" Représente une case avec un bonus";
-    tempReached += " |    \"" + CHARS[charSet][3] + "\" Représente la position du mineur";
-    tempScore += "|    \"   \" Représente une case déjà visitée";
-    tempScoreBonus += "|    \"" + CHARS[charSet][0] + "\" Représente une bombe";
+   // On inscrit des précisions sur les représentations au sein de la matrice a droite du statu
+   tempTarget += "|    \"" + CHARS[charSet][1] + "x" + CHARS[charSet][2] + "\" Représente une case avec un bonus";
+   tempReached += " |    \"" + CHARS[charSet][3] + "\" Représente la position du mineur";
+   tempScore += "|    \"   \" Représente une case déjà visitée";
+   tempScoreBonus += "|    \"" + CHARS[charSet][0] + "\" Représente une bombe";
 
-    // On affiche le tout
-    std::cout << model->toString(charSet) << std::endl
-              << std::endl
-              << tempTarget << std::endl
-              << tempReached << std::endl
-              << tempScore << std::endl
-              << tempScoreBonus << std::endl
-              << std::endl
-              << levelStr << std::endl
-              << lifeStr << std::endl << std::endl;
+   // On affiche le tout
+   std::cout << model->toString(charSet) << std::endl
+	     << std::endl
+	     << tempTarget << std::endl
+	     << tempReached << std::endl
+	     << tempScore << std::endl
+	     << tempScoreBonus << std::endl
+	     << std::endl
+	     << levelStr << std::endl
+	     << lifeStr << std::endl << std::endl;
 }
 
 
 int textMain(int charSet)
 {
    /*for(unsigned i = 1; i < 20; i++)
+     {
+     std::cout << i << "::" << typeToString<int>(inv(i)*100) << "% -> ";
+     }
+     std::cout << std::endl;*/
+
+
+   srand(time(NULL));
+
+   Board *realModel;
+   Board *modelForGame;
+   GameState state = CONTINUE;
+   unsigned lifes = 5;
+   unsigned bonusLifes = 0;
+   unsigned level = 1;
+
+   realModel = new Board(18, 18, inv(level), 22);
+
+   while(state != QUIT)
    {
-      std::cout << i << "::" << typeToString<int>(inv(i)*100) << "% -> ";
+      modelForGame = new Board(*realModel);
+
+      printModel(modelForGame, lifes, level, charSet);
+      state = menu(modelForGame, lifes);
+
+      while(state == CONTINUE && lifes > 0) // Tantque le jeu continu => Le joueur ne souhaite pas quitter et il n'a pas perdu
+      {
+	 printModel(modelForGame, lifes, level, charSet);
+	 state = menu(modelForGame, lifes);
+	 if(modelForGame->getBonusLifes() > bonusLifes)
+	 {
+	    lifes += modelForGame->getBonusLifes() - bonusLifes;
+	    bonusLifes = modelForGame->getBonusLifes();
+	 }
+      }
+
+      if((lifes < 1 && state != QUIT) || state == WON)
+      {
+	 if(state != WON)
+	 {
+	    lifes = 5;
+	    level = 0;
+	 }
+	 delete realModel;
+	 level++;
+
+	 realModel = new Board(18, 18, inv(level), 22);
+	 bonusLifes = 0;
+      }
+
+      delete modelForGame;
    }
-   std::cout << std::endl;*/
 
+   delete realModel;
 
-    srand(time(NULL));
-
-    Board *realModel;
-    Board *modelForGame;
-    GameState state = CONTINUE;
-    unsigned lifes = 5;
-    unsigned bonusLifes = 0;
-    unsigned level = 1;
-
-    realModel = new Board(18, 18, inv(level), 22);
-
-    while(state != QUIT)
-    {
-        modelForGame = new Board(*realModel);
-
-        printModel(modelForGame, lifes, level, charSet);
-        state = menu(modelForGame, lifes);
-
-        while(state == CONTINUE && lifes > 0) // Tantque le jeu continu => Le joueur ne souhaite pas quitter et il n'a pas perdu
-        {
-            printModel(modelForGame, lifes, level, charSet);
-            state = menu(modelForGame, lifes);
-            if(modelForGame->getBonusLifes() > bonusLifes)
-            {
-                lifes += modelForGame->getBonusLifes() - bonusLifes;
-                bonusLifes = modelForGame->getBonusLifes();
-            }
-        }
-
-        if((lifes < 1 && state != QUIT) || state == WON)
-        {
-            if(state != WON)
-            {
-                lifes = 5;
-                level = 0;
-            }
-            delete realModel;
-            level++;
-
-            realModel = new Board(18, 18, inv(level), 22);
-        }
-
-        delete modelForGame;
-    }
-
-    delete realModel;
-
-    return EXIT_SUCCESS;
+   return EXIT_SUCCESS;
 }
 
