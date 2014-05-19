eb68bec0acd23c4fce3d55d4ab0402b650a4db5c
Nicolas Gomez <nicolas.gomez@etu.u-bordeaux1.fr>
2014-05-19 18:59:52 +0200

    J'espère que j'ai bien mergé vérifie

--- sources/includes/Board.hh
+++ sources/includes/Board.hh
@@ -27,16 +27,12 @@ struct point
     unsigned y;
 };
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 8cdafa96a618da366be8308e3ea605d892749688
 // représente le plateau de jeu (model du MVC) ; voir le rapport de conception pour quelques explications
 class Board
 {
 public:
-<<<<<<< HEAD
     struct change
     {
         enum
@@ -48,87 +44,45 @@ public:
             ,LOST
             ,WON
             ,REPLACE
+            ,DESTRUCT
 
         } type;
 
+        point location;
+
         union
         {
-            unsigned value;
-            // SCORE* et LIFE*
-
-            point location;
-            // MOVE
-
             struct
             {
-                point location;
-                const Square * square;
-=======
-   struct change
-   {
-      enum
-      {
-	 SCORE
-	 ,SCORE_BONUS
-	 ,LIFE_BONUS
-	 ,MOVE
-	 ,LOST
-	 ,WON
-	 ,REPLACE
-	 ,DESTRUCT
-	 
-      } type;
+                unsigned delta;
+                unsigned total;
 
-      point location;
+            } value;
+            // SCORE* LIFE* MOVE (Reached)
 
-      union
-      {
-	 struct
-	 {
-	    unsigned delta;
-	    unsigned total;
-
-	 } value;
-	 // SCORE* LIFE* MOVE (Reached)
-
-	 Square * square;
-	 // REPLACE
->>>>>>> 8cdafa96a618da366be8308e3ea605d892749688
-
-            } square;
+            Square * square;
             // REPLACE
 
-<<<<<<< HEAD
-        } infos;
-    };
+        } square;
+    }
 
-    typedef void (*observer)(const change &);
-=======
-   class Observer
-   {
-   private:
-      std::queue<Board::change> Changes;
-   protected:
-      void pop();
-      const Board::change * front();
-   public:
-      void push(const Board::change&);
-      virtual bool care(const Board::change&);
-   };
->>>>>>> 8cdafa96a618da366be8308e3ea605d892749688
+    class Observer
+    {
+    private:
+        std::queue<Board::change> Changes;
+    protected:
+        void pop();
+        const Board::change * front();
+    public:
+        void push(const Board::change&);
+        virtual bool care(const Board::change&);
+    };
 
 
 
 private:
-<<<<<<< HEAD
     std::vector< std::vector<Square*> > Squares;
-    std::set<observer> Observers;
-
-    point Digger;
-=======
-   std::vector< std::vector<Square*> > Squares;
-   std::set<Observer*> Observers;
->>>>>>> 8cdafa96a618da366be8308e3ea605d892749688
+    std::set<Observer*> Observers;
 
     unsigned Target;
     unsigned Reached;
@@ -149,7 +103,7 @@ private:
 
     void notify(const change&) const;
 
-    // temporaire ; la génération aléatoire sera plus complexe
+// temporaire ; la génération aléatoire sera plus complexe
     Square* newRandomSquare(double difficulty, unsigned longestside);
 
 public:
@@ -162,7 +116,7 @@ public:
 
     GameState move(int dx, int dy);
 
-    // interface pour Square
+// interface pour Square
     void addScore(unsigned score);
     void addBonusScore(unsigned score);
     void addBonusLifes(unsigned lifes);
@@ -171,9 +125,7 @@ public:
                ,int dx = 0, int dy = 0, int distance = -1);
     void replaceSquare(int x, int y, Square * newone);
 
-
-<<<<<<< HEAD
-    // accesseurs
+// accesseurs
     unsigned getTarget()  const;
     unsigned getReached() const;
     unsigned getScore()      const;
@@ -182,29 +134,14 @@ public:
     point    getDigger() const;
     unsigned getWidth()  const;
     unsigned getHeight() const;
+    const Square * getSquare(unsigned x, unsigned y) const;
 
 
-    void registerObserver(observer o);
-    void unregisterObserver(observer o);
-=======
-   // accesseurs
-   unsigned getTarget()  const;
-   unsigned getReached() const;
-   unsigned getScore()      const;
-   unsigned getBonusScore() const;
-   unsigned getBonusLifes() const;
-   point    getDigger() const;
-   unsigned getWidth()  const;
-   unsigned getHeight() const;
-   const Square * getSquare(unsigned x, unsigned y) const;
+    void registerObserver(Observer* o);
+    void unregisterObserver(Observer* o);
 
 
-   void registerObserver(Observer* o);
-   void unregisterObserver(Observer* o);
->>>>>>> 8cdafa96a618da366be8308e3ea605d892749688
-
-
-    // pour tests / affichage en mode texte uniquement ; n'entre pas dans la logique de la conception
+// pour tests / affichage en mode texte uniquement ; n'entre pas dans la logique de la conception
     const std::string toString(const int &charSet) const;
 };
 
