66e721d837a63ae178d476100a7c4946efa220c2
Timothée Jourde <tim@localhost.localdomain>
2014-05-25 19:14:26 +0200

    corrections

--- sources/EventHandler.cc
+++ sources/EventHandler.cc
@@ -15,35 +15,42 @@ EventHandler::~EventHandler()
 {}
 
 
-#define MAY_INTERRUPT( x ) { int up; if ( (up = (x)) ) return up; }
+#define MAY_INTERRUPT( x ) if ( (up = (x)) ) goto interrupt
 
 int EventHandler::run(sf::RenderWindow & w)
 {
-   w.SetView(View);
-   View.SetCenter(0,0);
-   View.SetHalfSize(w.GetWidth()/2.f, w.GetHeight()/2.f);
+   const sf::View & orig  = w.GetView();
+   const sf::View deforig = w.GetDefaultView();
 
+   w.SetView(w.GetDefaultView());
+   w.GetDefaultView().SetCenter(0,0);
+   w.GetDefaultView().SetHalfSize(w.GetWidth()/2.f, w.GetHeight()/2.f);
 
+   int up;
    while (true)
    {
-      const float now = Clock.GetElapsedTime();
-
       sf::Event e;
       while (w.GetEvent(e))
-	 MAY_INTERRUPT( call(w,e,now) );
+	 MAY_INTERRUPT( call(w,e, Clock.GetElapsedTime()) );
 
-      MAY_INTERRUPT( tick(w,now) );
+      MAY_INTERRUPT( tick(w, Clock.GetElapsedTime()) );
 
 
       if (Need_refresh)
       {
 	 w.Clear(sf::Color(80,80,80));
-
-	 draw(w,now);
+	 
+	 draw(w, Clock.GetElapsedTime());
 	 w.Display();
 	 Need_refresh = false;
       }
-   }
+      
+   } interrupt:
+
+
+   w.GetDefaultView() = deforig;
+   w.SetView(orig);
+   return up;
 }
 
 int EventHandler::call(sf::RenderWindow & w, sf::Event & e, float now)
@@ -79,10 +86,10 @@ int EventHandler::call(sf::RenderWindow & w, sf::Event & e, float now)
    }
 }
 
-sf::View& EventHandler::getHandlerView()
-{
-    return View;
-}
+// sf::View& EventHandler::getHandlerView()
+// {
+//     return View;
+// }
 
 void EventHandler::need_refresh()
 {
@@ -136,7 +143,7 @@ int EventHandler::text_entered(sf::RenderWindow & w, sf::Event::TextEvent e, flo
 int EventHandler::resized(sf::RenderWindow & w, sf::Event::SizeEvent & e, float now)
 {
    // éviter l'auto-resize chelou
-   View.SetHalfSize(w.GetWidth()/2.f, w.GetHeight()/2.f);
+   w.GetDefaultView().SetHalfSize(w.GetWidth()/2.f, w.GetHeight()/2.f);
 
    need_refresh();
    return 0;
